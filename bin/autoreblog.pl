9cafb46e819bd255b7abae90dbd9ba403ecf85b7
punytan <punytan@gmail.com>
2012-05-06 06:57:32 +0900

    configurable account path

--- bin/autoreblog.pl
+++ bin/autoreblog.pl
@@ -5,6 +5,8 @@ use Encode;
 use App::AutoReblog;
 use Config::PP;
 
+$Config::PP::DIR = $ARGV[0] || "$ENV{HOME}/.ppconfig";
+
 my $google = config_get "google.com";
 my $tumblr = config_get "tumblr.com";
 
